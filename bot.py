fcb72226f6a76fcc28f70118b7e427f1e290c70d
PyushSingh2008 <Pyusharya01@gmail.com>
2026-06-28 22:09:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,15 +3,16 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, ChatJoinRequestHandler
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(name)
+logger = logging.getLogger(__name__)
 
 BOT_TOKEN = "8949333597:AAFm6fM1JFkWJzWhzONUF8U3GDqupejCQF8"
 CHANNEL_ID = -1002182251619
 IMAGE_URL = "https://ibb.co/B5QXY1B8/photo-2026-05-15-22-03-17.jpg"
+
 WELCOME_MESSAGE = (
     "Qoutex Compounding series group click this link 🔗 👇\n\n"
-    "https://t.me/+YsiZbHY4b4MwMGY9"
-    "https://t.me/+YsiZbHY4b4MwMGY9"
+    "https://t.me/+YsiZbHY4b4MwMGY9\n"
+    "https://t.me/+YsiZbHY4b4MwMGY9\n\n"
     "🔥 Just 7 Days Challenge! 🔥\n"
     "Turn $30 → $1000 💸\n"
     "Limited slots — Join Fast & Start Earning Now! 🚀"
@@ -39,18 +40,6 @@ def main():
     app.add_handler(ChatJoinRequestHandler(handle_join_request))
     logger.info("Bot started 24/7!")
     app.run_polling(drop_pending_updates=True)
-
-if name == "main":
-    main()            )
-        logger.info(f"Welcomed: {user.first_name} ({user.id})")
-    except Exception as e:
-        logger.error(f"Error: {e}")
-
-def main():
-    app = Application.builder().token(BOT_TOKEN).build()
-    app.add_handler(ChatJoinRequestHandler(handle_join_request))
-    logger.info("Bot started...")
-    app.run_polling()
 
 if __name__ == "__main__":
     main()
